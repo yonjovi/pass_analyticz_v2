ee8ca7874c034a7abb96410465c096bf10086bbc
Yonatan Rotem <yonatanrotem@Yonatans-Mac-Studio.local>
2022-05-28 02:06:27 +1000

    initial commit

--- streamlitz.py
+++ streamlitz.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 import pandas as pd
 import numpy as np
 import pyphen
@@ -7,12 +8,12 @@ import requests
 import hashlib
 from plotly.offline import plot
 import plotly.graph_objs as go
-
+import random
 
 header = st.container()
 dataset = st.container()
-features = st.container()
-model_training = st.container()
+generator = st.container()
+contact = st.container()
 
 df = pd.read_csv('password_analyticz.csv', index_col='Password')
 
@@ -27,6 +28,7 @@ class PasswordAnalytics:
                 if vowel.upper() in char.upper():
                     vowel_count += 1
                     vowel_chars.append(char)
+
         # print("Vowel count: ", vowel_count)
         # print("Vowels in password: ", vowel_chars)
         df.loc["Pass"]["num_vowels"] = vowel_count
@@ -37,10 +39,17 @@ class PasswordAnalytics:
         a = pyphen.Pyphen(lang='en')
         syllables_str = a.inserted(word)
         split_syl = syllables_str.split("-")
+        nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
         # print("Syllable count: ", len(split_syl))
-        # print("Syllables: ", split_syl)
+        print("Syllables: ", split_syl)
         df.loc["Pass"]["num_syllables"] = len(split_syl)
-        # print(df.loc["Pass"]["num_syllables"])
+        # for i in split_syl:
+        #     for num in nums:
+        #         if num in num_str:
+        #             split_syl.remove(num)
+        #             df.loc["Pass"]["num_syllables"] = len(split_syl)
+        # # print(df.loc["Pass"]["num_syllables"])
         # df.to_csv(f'{word}_analytics.csv')
         df.to_csv('password_analyticz.csv')
 
@@ -84,6 +93,8 @@ class PasswordAnalytics:
         df.loc["Pass"]["num_chars"] = len(char_list)
         # df.to_csv(f'{word}_analytics.csv')
         df.to_csv('password_analyticz.csv')
+        if df.loc["Pass"]["num_chars"] == 0:
+            df.loc["Pass"]["num_syllables"] = 0
 
     def upper_counter(word):
         upper_list = []
@@ -128,12 +139,18 @@ class PasswordAnalytics:
             pwned_hash = pwned_pass.split(":")
             pwned_dict[pwned_hash[0]] = pwned_hash[1]
 
+
+
         if sha_postfix in pwned_dict.keys():
             print("Password has been compromised {0} times".format(pwned_dict[sha_postfix]))
-            st.write("Password has been compromised {0} times ☠️".format(pwned_dict[sha_postfix]))
+            compromise_count = int(pwned_dict[sha_postfix])
+            formatted_count = '{:>12,.0f}'.format(compromise_count)
+            st.write(f"Password has been compromised {formatted_count} times ☠️")
+            #st.write("Password has been compromised {0} times ☠️".format(pwned_dict[sha_postfix]))
+            print(pwned_dict[sha_postfix])
             print('')
         else:
-            st.balloons()
+            # st.balloons()
             st.write("Password is safe!!!")
             print("Password is safe!")
             print('')
@@ -162,6 +179,7 @@ class PlotAnalytics:
         y6 = df.loc[1]['num_special']
         y7 = df.loc[1]['num_vowels']
         y8 = df.loc[1]['num_syllables']
+        y9 = df.loc[1]['Password']
 
         ## PLOTLY EXAMPLE ##
         average = [x1, x2, x3, x4, x5, x6, x7, x8]
@@ -170,7 +188,7 @@ class PlotAnalytics:
         trace1 = go.Bar(
             x=x,
             y=average,
-            name='Average'
+            name='Worst Passwords'
         )
 
         trace2 = go.Bar(
@@ -187,13 +205,75 @@ class PlotAnalytics:
         fig = go.Figure(data=data, layout=layout)
         st.plotly_chart(fig, use_container_width=True)
 
+        color_discrete_sequence = ['', '#92B4EC', '#FFE69A', '#9FB4FF', '#6FB2D2', '#85C88A', '#99FFCD', '#614124',
+                                   '#EEEEEE']
+
+        fig2 = go.Figure(go.Sunburst(
+            labels=["Password", "Length", "Characters", "Digits", "Uppercase", "Lowercase", "Special", "Vowels", "Syllables"],
+            parents=["", "Password", "Length", "Length", "Characters", "Characters", "Characters", "Characters", "Characters"],
+            values=[y9, y1, y2, y3, y4, y5, y6, y7, y8],
+            marker=dict(colors=color_discrete_sequence)
+        ))
+        fig2.update_layout(margin = dict(t=0, l=0, r=0, b=0))
+        st.plotly_chart(fig2, use_container_width=True)
+
+
+def generate_pass(password_length, password_amount):
+    uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowercase_letters = uppercase_letters.lower()
+    digits = "01234567890"
+    symz = list(set(punctuation))
+    symbols = ""
+    for i in symz:
+        symbols += i
+    # print(symbols)
+
+    upper, lower, nums, syms = True, True, True, True
+
+    all = ""
+
+    if upper:
+        all += uppercase_letters
+    if lower:
+        all += lowercase_letters
+    if nums:
+        all += digits
+    if syms:
+        all += symbols
+
+    length = password_length
+    amount = password_amount
+
+    for x in range(amount):
+        gen_password = "".join(random.sample(all, length))
+        st.write(x+1, ". ", gen_password)
+        print(gen_password)
+
+
+# with st.sidebar:
+#     selected = option_menu(
+#         menu_title=None,
+#         options=["Password Analysis", "Password Generator", "Contact Me"],
+#         icons=["house", "book", "envelope"],
+#         menu_icon="cast",
+#         default_index=0,
+#     )
+
+
+st.sidebar.markdown('''
+# Sections
+- [Password Analysis](#password-analysis)
+- [Password Generator](#password-generator)
+- [Contact Me](#contact-me)
+''', unsafe_allow_html=True)
+
 with header:
     st.title('Hello!')
     st.text('In this project I look into password security and analysis.')
 
 with dataset:
-    st.header('Password analysis:')
-    st.text("Analyse any password! Enter below")
+    st.header('Password Analysis')
+    st.text("Analyse any password! Enter one below...")
     password = st.text_input('Enter a password: ', type="password")
 
     PasswordAnalytics.pass_length(password)
@@ -207,14 +287,34 @@ with dataset:
     PasswordAnalytics.is_pass_safe(password)
 
     PlotAnalytics.group_bar()
+    st.write("")
+    st.write("")
+
+    #generate_pass(pass_length, pass_amount)
+
+
+
 
     # pass_data = pd.read_csv('password_analyticz.csv')
     # st.write(pass_data.head())
     # pass_length_data = pd.DataFrame(pass_data['Length'])
     # st.bar_chart(pass_length_data)
 
-with features:
-    st.header('')
+# with generator:
+    st.header('Password Generator')
+    st.text('Use the sliders below to generate as many passwords as you like!')
+    pass_length = st.slider('Choose password length:', 1, 50, 6)
+    st.write("Password length: ", pass_length, ".")
+    pass_amount = st.slider('Choose how many passwords you would like to generate:', 1, 100, 1)
+    st.write("Passwords to be generated: ", pass_amount, ".")
+    st.button("Generate Again!", on_click=generate_pass(pass_length, pass_amount))
+    st.write("")
+    st.write("")
 
-with model_training:
-    st.header('')
+with contact:
+    st.header('Contact Me')
+    st.write('Email: yonpython@gmail.com')
+    st.write("")
+    st.write("")
+
+
